9e4f163d5c72e76874b85f56b7639c40f4118221
Totiruzi <totiruzi@yahoo.com>
2020-11-24 13:10:49 +0300

    Reflect updates in real time

--- src/utilities/firebase.js
+++ src/utilities/firebase.js
@@ -16,5 +16,7 @@ var firebaseConfig = {
 
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  export const chatsRef = db.ref('chats')
 
 export default firebase;
